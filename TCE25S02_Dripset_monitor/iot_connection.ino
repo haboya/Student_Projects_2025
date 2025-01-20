34ee9b52a1b5b5dec0ce5468cb28d746ef25ad16
Emmanuel Haboya <haboyaemmanuel@gmail.com>
2025-01-20 08:31:31 +0300

    Changed IOT protocol form Thingspeak to Blynk

--- TCE25S02_Dripset_monitor/iot_connection.ino
+++ TCE25S02_Dripset_monitor/iot_connection.ino
@@ -1,58 +1,43 @@
 
 #include "dripset_monitor_config.h"
 #include <ESP8266WiFi.h>
-#include <ThingSpeak.h>
+#include <BlynkSimpleEsp8266.h>
 
 const char* ssid = "PowerUPLab";
 const char* password = "#powerup2"; 
 
-WiFiClient  client;
+BlynkTimer timer;
 
-unsigned long myChannelNumber = 1675288;
-const char * myWriteAPIKey = "69WWTWVWTZE1YEG0";
+bool iot_data_sent = false;
 
-// Timer variables
-unsigned long lastTime = 0;
-unsigned long timerDelay = 30000;
+void iot_sendData( void )
+{
+    Blynk.virtualWrite(V0, dripset_params.drip_running);
+    Blynk.virtualWrite(V1, dripset_params.drip_flow_rate);
+    Blynk.virtualWrite(V2, dripset_params.drip_volume_left);
 
-bool iot_connected = false;
+    iot_data_sent = true;
+}
 
 bool iot_Init( void )
 {
-    WiFi.mode(WIFI_STA);   
-    ThingSpeak.begin(client);  // Initialize ThingSpeak
+    Blynk.begin(BLYNK_AUTH_TOKEN, ssid, password);
+    timer.setInterval(1000L, iot_sendData);
 
     return true;
 }
 
 bool iot_UpdateStatus( void )
 {
-    static unsigned long prev_send_millis = 0;
-    if(millis() - prev_send_millis < DRIPSET_IOT_UPDATE_INTERVAL*1000) return false;
+    Blynk.run();
+    timer.run();
 
-    // reconnect to wifi if disconnected
-    if(WiFi.status() != WL_CONNECTED)
+    if(iot_data_sent != false)
     {
-        WiFi.begin(ssid, password);
-        prev_send_millis = millis();
-        return false;
+        iot_data_sent = false;
+        return true;
     }
 
-    // set the fields with the values
-    ThingSpeak.setField(1, dripset_params.drip_flow_rate);
-    ThingSpeak.setField(2, dripset_params.drip_volume_left);
-    ThingSpeak.setField(3, dripset_params.drip_running);
-    
-    int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
-
-    if(x == 200){
-      Serial.println("Channel update successful.");
-    }
-    else{
-      Serial.println("Problem updating channel. HTTP error code " + String(x));
-    }
-
-    prev_send_millis = millis();
-    return true;
+    return false;
 }
 
